c3ca663ce3902ce2b4eb6843ebd05bd226f52f6a
Marcus Crafter <crafterm@redartisan.com>
2011-05-14 17:11:04 +1000

    Initial Commit

---
+++ IconApp/IconAppAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  IconAppAppDelegate.h
+//  IconApp
+//
+//  Created by Marcus Crafter on 14/05/11.
+//  Copyright 2011 Red Artisan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IconAppAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
